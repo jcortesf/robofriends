561b6245a82aaab0df6b0cfde41f7fc3a4ec044f
Julio Cortés <juliocortesf@gmail.com>
2020-04-11 23:29:33 -0400

    WP Clean Code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
-import './App.css';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import '../containers/App.css';
 
 class App extends Component{
 	constructor() {
@@ -26,10 +26,11 @@ class App extends Component{
 	}
 
 	render () {
-		const filteredRobots = this.state.robots.filter( robots => {
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots, searchfield } = this.state;
+		const filteredRobots = this.state.robots.filter( robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
-		if (this.state.robots.length ===0) {
+		if (!robots.length) {
 			return <h1>Loading</h1>
 		} else {
 			return (
